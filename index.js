34a2695e4de76b42e1f9e8825fc76b9f272e7df9
WildcardSearch <whiterhymes@yahoo.com>
2023-04-04 19:17:40 -0500

    Fixes #1
    exposes data and functions to the implementation

--- index.js
+++ index.js
@@ -8,8 +8,22 @@
 const TwitchChatBot = require("./lib/twitch-chat-bot.js");
 const TwitchChatBotModule = require("./lib/twitch-chat-bot-module.js");
 
+const errorCodes = require("./data/error-codes.js");
+const eventPriority = require("./data/event-priority.js");
+const moderation = require("./data/moderation.js");
+const time = require("./data/time.js");
+
+const functions = require("./functions.js");
+
 
 module.exports = {
 	TwitchChatBot: TwitchChatBot,
 	TwitchChatBotModule: TwitchChatBotModule,
+
+	errorCodes: errorCodes,
+	eventPriority: eventPriority,
+	moderation: moderation,
+	time: time,
+
+	functions: functions,
 };
