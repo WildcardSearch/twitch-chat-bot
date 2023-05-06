c84fc7c2c5d9e8110ae3ba79005b19538d223835
WildcardSearch <whiterhymes@yahoo.com>
2023-05-06 12:05:54 -0500

    For #54
    translate: docs (fix)

--- locales/en/docs.js
+++ locales/en/docs.js
@@ -1,12 +1,12 @@
 module.exports = {
 	"docs": {
-		"command_helpfile_description": "Produce this document.",
+		"command_helpfile_description": "Produce documentation for the chat bot.",
 		"command_list": {
 			"heading": "%{bold_start}%{display_name}%{bold_end} (%{instance_version})%{newline}%{italic_start}automatically generated command list%{italic_end}\n\n%{bold_start}%{permission_group_description}%{bold_start}",
 			"alias_count": "Alias |||| Aliases",
 			"alias_list": "%{newline}%{bold_start}%{title}:%{bold_end} %{alias_list}",
 			"usage_text": "%{newline}%{bold_start}Usage:%{bold_end} %{usage}",
 		},
-		"success": "Documentation generated.",
+		"success": "Documentation has been generated.",
 	},
 };
